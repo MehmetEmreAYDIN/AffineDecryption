0e6aee7852c4932d48778e71a138cc610540c895
wited <wited@LAPTOP-CO67K0CT>
2024-08-23 16:16:25 +0300

    Proje dosyası ekle.

---
+++ AffineDecryption/Program.cs
@@ -0,0 +1,18 @@
+﻿char[] alphabet = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ".ToCharArray();    //Enter letters of the alphabet in this field
+char[] cipherText = "ŞifreliMetin".ToUpper().ToCharArray();   //Enter cipher text in this field
+int a = 2; int b = 3;   //Enter the key parameters a and b in the field on the left
+string plainText = string.Empty;
+
+int d = 1;
+while (true)
+{
+    if ((alphabet.Length * d + 1) % a == 0) d = (alphabet.Length * d + 1) / a; break;
+    d++;
+}
+for (int i = 0; i < cipherText.Length; i++)
+{
+    int ndx = (Array.IndexOf(alphabet, cipherText[i]) - b) * d % alphabet.Length;
+while (ndx < 0) ndx += alphabet.Length;
+    plainText += alphabet[ndx];
+}
+Console.WriteLine(plainText);
