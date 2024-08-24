bf9f891a94abaa6942e19b9aa8a681e6790fd5c9
MehmetEmreAYDIN <witedea@gmail.com>
2024-08-24 11:53:24 +0300

    Update Program.cs

--- AffineDecryption/Program.cs
+++ AffineDecryption/Program.cs
@@ -1,6 +1,6 @@
-﻿char[] alphabet = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ".ToCharArray();    //Enter the letters of the alphabet into this field.
-char[] cipherText = "ŞifreliMetin".ToUpper().ToCharArray();   //Enter the cipher text into this field.
-int a = 2; int b = 3;   //Enter the key parameters a and b into the respective fields. 
+﻿char[] alphabet = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ".ToCharArray();    //Enter the letters of the alphabet in this field.
+char[] cipherText = "ŞifreliMetin".ToUpper().ToCharArray();   //Enter the cipher text in this field.
+int a = 2; int b = 3;   //Enter the key parameters a and b in the respective fields. 
 string plainText = string.Empty;
 
 int d = 1;
